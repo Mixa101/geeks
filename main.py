a981653f8b1d492326a66ce1c884a494b6f3df6f
Mixa101 <kirolkuro@gmail.com>
2024-10-15 15:22:36 +0600

    hw_4

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
 from aiogram import executor
 import logging
-from config import dp, bot, admins
-from handlers import commands, echo, quiz, game, FSM_reg
-import asyncio
+from config import dp
+from handlers import commands, echo, quiz, game, FSM_reg, fsm_store
 from db import db_main
 
 async def on_startup(_):
-    for admin in admins:
-        await bot.send_message(chat_id=admin, text='Бот запущен!')
-        await db_main.sql_create()
+    await db_main.sql_create()
 
-async def on_shutdown(_):
-    for admin in admins:
-        await bot.send_message(chat_id=admin, text='Бот отключен')
-
+fsm_store.register_store_handlers(dp) # регистрируем обработчики
 commands.register_handlers_commands(dp)
 quiz.register_handlers_quiz(dp)
 FSM_reg.register_fsm_handlers(dp)
 game.register_game_handlers(dp)
 echo.register_handler_echo(dp)
 
+
 if __name__=='__main__':
     logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup, on_shutdown=on_shutdown)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
